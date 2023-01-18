37940a3f4a30522d73bb78b20c08693bc364adeb
motiejus1 <motiejus.baliukonis@vka.lt>
2023-01-18 21:43:15 +0200

    *roles permission

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
@@ -16,6 +17,26 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::create([
+            'name' => 'Superadministratorius',
+            'email' => 'superadmin@localhost.lt',
+            'password' => Hash::make('password')
+        ]);
+
+        $role = Role::find(1);
+
+        $permission = Permission::pluck('id', 'id')->all();// sudeti teises ir roles i rysio lentele
+        $role->syncPermissions($permission);
+        $user->assignRole([$role->id]);
+        
+        //prie roles supedadmin priskirti visas teises
+        //role priskirti sio konkretaus vartotojo
+
+        // Slaptazodzio simboliu kiekis 8
+        // bent 1 didzioji raide
+        //bent skaicius
+        //specialus simbolis ,
+
+        //TOP 1000 slaptazodziu sarasa
     }
 }
